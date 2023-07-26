9249031196b2b5c5a19e4753ca4f1c93a03db409
hbashar434 <h.bashar434@gmail.com>
2023-07-26 20:26:42 +0600

    upto task1

---
+++ Task1.js
@@ -0,0 +1,11 @@
+function reverseString(inputString) {
+  let reversedString = "";
+  for (let i = inputString.length - 1; i >= 0; i--) {
+    reversedString += inputString[i];
+  }
+  return reversedString;
+}
+
+const input = "hello world";
+const output = reverseString(input);
+console.log(output);
