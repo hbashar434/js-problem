113a68784c225d0c5058519fb41197db5868a758
hbashar434 <h.bashar434@gmail.com>
2023-07-27 16:16:32 +0600

    upto task6

---
+++ Task6.js
@@ -0,0 +1,20 @@
+const generateRandomPassword = (length) => {
+  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  const numberChars = "0123456789";
+  const specialChars = "!@#$%&*+";
+
+  const allChars = uppercaseChars + lowercaseChars + numberChars + specialChars;
+
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * allChars.length);
+    password += allChars[randomIndex];
+  }
+
+  return password;
+};
+
+const passwordLength = 8;
+const randomPassword = generateRandomPassword(passwordLength);
+console.log(randomPassword);
