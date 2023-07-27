b4b7b8e2df916749e5faf573cd2f7caed0fcd6a4
hbashar434 <h.bashar434@gmail.com>
2023-07-27 16:01:12 +0600

    upto task4

---
+++ Task4.js
@@ -0,0 +1,21 @@
+const findTwoNumbersWithSum = (arr, target) => {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    const sum = arr[left] + arr[right];
+
+    if (sum === target) {
+      return [left, right];
+    } else if (sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return [];
+};
+
+const indices = findTwoNumbersWithSum([1, 3, 6, 8, 11, 15], 9);
+console.log(indices);
